71b78195f5f2798b5b046e75e9580083a8197dfd
AndresBarrios18 <pipe292011@gmail.com>
2024-02-11 21:44:56 -0500

    Se crea la primera Version del prograba con la clase Ave

---
+++ src/main/java/com/mycompany/ave/Avestruz.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ave;
+
+/**
+ *
+ * @author pipe2
+ */
+public class Avestruz extends Ave {
+    
+    public void makeSound(){
+        System.out.println("<<Silencio>>");
+    
+    }
+    
+}
---
+++ src/main/java/com/mycompany/ave/Gallina.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ave;
+
+/**
+ *
+ * @author pipe2
+ */
+public class Gallina extends Ave {
+    
+    public void makeSound(){
+    
+        System.out.println("Grasna");
+    
+    }
+    
+}
